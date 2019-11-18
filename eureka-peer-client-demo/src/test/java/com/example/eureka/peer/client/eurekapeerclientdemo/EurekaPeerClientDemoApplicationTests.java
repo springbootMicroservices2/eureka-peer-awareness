5d959c5d8b39c3d02186789e5208c71ad23f844a
springbootMicroservices2 <57837154+springbootMicroservices2@users.noreply.github.com>
2019-11-18 06:58:35 +0530

    initial

---
+++ eureka-peer-client-demo/src/test/java/com/example/eureka/peer/client/eurekapeerclientdemo/EurekaPeerClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka.peer.client.eurekapeerclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaPeerClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-peer-demo/src/test/java/com/example/eureka/peer/eurekapeerdemo/EurekaPeerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka.peer.eurekapeerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaPeerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
